782a5fd15c16bf8eeb088be7253c1e30e2a1dec6
Mr_Black <andrey-ch2001@mail.ru>
2023-11-30 22:00:11 +0300

    beauty

---
+++ convertations.c
@@ -0,0 +1,13 @@
+#include "decimal.h"
+
+// перевести из int в decimal (всегда успешно)
+int s21_from_int_to_decimal(int src, s21_decimal *dst){
+    *dst = init_decimal(
+    src<0 ? ~(src) + 1 : src,
+    0,
+    0, 
+    src<0 ? 1:0, 
+    0
+    );
+    return 0;
+}
--- decimal.c
+++ decimal.c
@@ -1,27 +1,5 @@
 #include "decimal.h"
 
-
-// узнать значение конкретного бита
-int get_bit(s21_decimal num, int cur_bit) {
-  int bit;
-  if ((num.bits[cur_bit / 32] & (1 << cur_bit % 32)) == 0) {
-    bit = 0;
-  } else {
-    bit = 1;
-  }
-
-  return bit;
-}
-
-// установить значение конкретного бита
-void set_bit(s21_decimal* num, int bit, int result) {
-  if (result == 1) {
-    num->bits[bit / 32] = num->bits[bit / 32] | (1 << bit % 32);
-  } else if (result == 0) {
-    num->bits[bit / 32] = num->bits[bit / 32] & ~(1 << bit % 32);
-  }
-}
-
 // распечатаем наш decimal от 0 до 127
 void print_decimal (s21_decimal num){
     for (int i = 3; i >= 0; i--) {
@@ -36,11 +14,6 @@ void print_decimal (s21_decimal num){
     }
 } 
 
-// устанавливает минус
-void set_minus(s21_decimal* num){
-   set_bit(num, 127, 1);
-}
-
 // инициализируем децимал
 s21_decimal init_decimal(int i1, int i2, int i3, int sign, int exp){
     s21_decimal d;
@@ -51,39 +24,6 @@ s21_decimal init_decimal(int i1, int i2, int i3, int sign, int exp){
     return d;
 }
 
-int s21_from_int_to_decimal(int src, s21_decimal *dst){
-    *dst = init_decimal(
-    src<0 ? ~(src) + 1 : src,
-    0,
-    0, 
-    src<0 ? 1:0, 
-    0
-    );
-    return 0;
-}
-
-// узнать значение scale коэфициента масштабирования
-int get_scale (s21_decimal num) {
-    int mask = 0b11111111;
-    mask <<= 16;
-    mask = num.bits[3] & mask;
-    mask >>= 16;
-    return mask;
-}
-
-// установить значение scale коэфициента масштабирования, 0 - успешно, 1 - еррор
-int set_scale (s21_decimal * num, int scale) {
-    int result = 0;
-    if (scale < 256 && scale >=0) {
-        for (int i = 0; i < 8; i++) {
-            set_bit(num, i + 16 + 96, scale % 2);
-            scale >>= 1;
-        }
-    } else {
-        result = 1;
-    }
-    return result;
-}
 
 
 int main(){
