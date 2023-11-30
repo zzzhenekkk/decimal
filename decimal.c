1d8580b6b602cd55fc0675e114c0d46711e5e0ab
Turkeysn <89052742425@mail.ru>
2023-11-30 19:19:31 +0300

    готов принт

--- decimal.c
+++ decimal.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <limits.h>
+
 
 typedef struct 
 {
@@ -30,44 +27,19 @@ void set_bit(s21_decimal* num, int bit, int result) {
   }
 }
 
-
-void from_10_to_2(int n) {
-    if (n == 0)  return;
-    from_10_to_2(n/2);
-    printf("%d", n%2);
-}
-
-void show_decimal(s21_decimal *d){
-    
-}
-
-
-int s21_from_int_to_decimal(int src, s21_decimal *dst){
-    int i = 0;
-    src = abs(src);
-    while(i<96 && src > 0){
-        int bits_grade = i/32;
-        dst->bits[bits_grade] += pow(10,i)*(src%2);
-        src/=2;
-        i++;
+// распечатаем наш decimal от 0 до 
+void print_decimal (s21_decimal* num){
+    for (int i = 0; i < 4; i++) {
+        printf("[");
+        for (int j = 0; j < 32; i++) {
+            printf("%d", get_bit(*num, i*32 + j));
+        }
+        printf("] ");
     }
-    for(int j = 0; j < i; j++){
-        
-    }
-    printf("%i\n", dst->bits[0]);
-    printf("%i\n", dst->bits[1]);
-    printf("%i", dst->bits[2]);
-    return 1;
 }
-
 
 int main(){
-    s21_decimal *dst;
-    for(int i = 0; i<4; i++)
-        dst->bits[i] = 0;
-    int a = 7;
-    // printf("%i", a<<3);
-    from_10_to_2(-3);
-    // int res = s21_from_int_to_decimal(~(0b111), dst);
+    s21_decimal dec;
+    print_decimal(&dec);
     return 0;
 }
