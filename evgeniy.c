3b7ecb8bfa5078f2ab5286deb3f5fc54dd88c4a8
Turkeysn <89052742425@mail.ru>
2023-12-05 22:42:30 +0300

    999

--- evgeniy.c
+++ evgeniy.c
@@ -5,15 +5,16 @@ int main() {
   s21_decimal val2 = {0};
   s21_decimal result = {0};
 
-  s21_from_int_to_decimal(123, &val1);
-  s21_from_int_to_decimal(-4, &val2);
+  s21_from_int_to_decimal(-123, &val1);
+  s21_from_int_to_decimal(4, &val2);
 
-  set_scale(&val1, 2);
+  // set_scale(&val1, 2);
 
   print_decimal(val1);
   print_decimal(val2);
 
-  printf("\n!!!%d!!!\n", s21_div(val1, val2, &result));
+  printf("\n!!!%d!!!\n", s21_add(val1, val2, &result));
+  // printf("\n!!!%d!!!\n", s21_div(val1, val2, &result));
 
   print_decimal(result);
 
