325adf294f570f39c6191026a73638317bebb6cd
Mr_Black <andrey-ch2001@mail.ru>
2023-12-06 12:00:01 +0300

    pull done

--- s21_decimal.c
+++ s21_decimal.c
@@ -27,8 +27,11 @@ s21_decimal init_decimal(int i1, int i2, int i3, int sign, int exp) {
 }
 
 // big_decimal* mul_mantissa(big_decimal a, big_decimal b, big_decimal* res) {
-//   for (int i = 0; i <= b.zero_left; i++){
-
+//   for (int i = 0; i <= b.one_position_left; i++){
+//     int digit = get_bit_big(&b,i);
+//     if (digit) {
+      
+//     }
 //   }
 // }
 
