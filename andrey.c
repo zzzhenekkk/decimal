fe0670f57fde08c13236eca374b809f57e4f6605
MrBlackDrs <andrey-ch2001@mail.ru>
2023-12-07 00:15:41 +0300

    comparision done

--- andrey.c
+++ andrey.c
@@ -1,24 +1,41 @@
 #include "s21_decimal.h"
 
 int main() {
-  s21_decimal dec1 = {0}, dec2 = {0};
+   s21_decimal dec1 = {0}, dec2 = {0};
+
+  /* test division */
+  // dec1 = init_decimal(9, 0, 0, 0, 3);
+  // dec2 = init_decimal(1, 0, 0, 0, 2);
+  // big_decimal big1 = {0}, big2 = {0}, res = {0};
+  // init_big(dec1, &big1);
+  // init_big(dec2, &big2);
+  // print_big_decimal(&big1);
+  // print_big_decimal(&big2);
+  // division(big1, big2, &res);
+  // print_big_decimal(&res);
+
+  /* test comparision */
   dec1 = init_decimal(9, 0, 0, 0, 3);
-  // set_minus(&dec1, 1);
-  dec2 = init_decimal(1, 0, 0, 0, 2);
-  big_decimal big1 = {0}, big2 = {0}, res = {0};
-  init_big(dec1, &big1);
-  init_big(dec2, &big2);
-  // printf("%d", get_sign(dec1));
-  // print_decimal(dec1);
-  // print_decimal(dec2);
-  // s21_is_less(dec1, dec2);
-  // big_decimal big = {0};
-  // init_big(dec1, &big);
-  print_big_decimal(&big1);
-  print_big_decimal(&big2);
+  dec2 = init_decimal(9, 0, 1, 0, 3);
+  print_decimal(dec1);
+  print_decimal(dec2);
 
+  int res = s21_is_less(dec1, dec2);
+  printf("dec1 < dec2 : %d\n", res);
 
-  division(big1, big2, &res);
-  print_big_decimal(&res);
+  res = s21_is_less_or_equal(dec1, dec2);
+  printf("dec1 <= dec2 : %d\n", res);
+
+  res = s21_is_greater(dec1, dec2);
+  printf("dec1 > dec2 : %d\n", res);
+
+  res = s21_is_greater_or_equal(dec1, dec2);
+  printf("dec1 >= dec2 : %d\n", res);
+
+  res = s21_is_equal(dec1, dec2);
+  printf("dec1 == dec2 : %d\n", res);
+
+  res = s21_is_not_equal(dec1, dec2);
+  printf("dec1 != dec2 : %d\n", res);
   return 0;
 }
--- s21_comparisions.c
+++ s21_comparisions.c
@@ -1,26 +1,27 @@
 #include "s21_decimal.h"
 
-void shift(big_decimal *x) {
-    int extra_bit_prev = 0, extra_bit_cur = 0;
-    for (int i = 0; i < 7; i++) {
-        extra_bit_prev =  extra_bit_cur;
-        extra_bit_cur = get_bit_big(x, i*32 + 31);
-        x->bits[i]<<=1;
-        // extra_bit_cur = get_bit_big(x, (i+1)*32 + 31);
-        set_bit_big(x, i*32, extra_bit_prev);
-    }
-    if (extra_bit_cur == 1)
-        printf("выход за пределы мантиссы");
-}
+// void shift(big_decimal *x) {
+//     int extra_bit_prev = 0, extra_bit_cur = 0;
+//     for (int i = 0; i < 7; i++) {
+//         extra_bit_prev =  extra_bit_cur;
+//         extra_bit_cur = get_bit_big(x, i*32 + 31);
+//         x->bits[i]<<=1;
+//         // extra_bit_cur = get_bit_big(x, (i+1)*32 + 31);
+//         set_bit_big(x, i*32, extra_bit_prev);
+//     }
+//     if (extra_bit_cur == 1)
+//         printf("выход за пределы мантиссы");
+// }
 
 int comparator(s21_decimal *x1, s21_decimal *x2) {
     // 1 : x1 > x2
     // 0 : x1 == x2
     // -1 : x1 < x2
-    // 0 0, 1 1, 1 0 , 0 1
+    // варианты знаков: 0 0, 1 1, 1 0 , 0 1
     int res = 0;
     int sign1 = get_sign(*x1), sign2 = get_sign(*x2);
-    int both_negative = 0, both_positive = 0;
+    int both_negative = 0;
+    // int both_positive = 0;
 
     // проверка чисел на разные знаки
     if (sign1 == 1 && sign2 == 0)
@@ -29,30 +30,42 @@ int comparator(s21_decimal *x1, s21_decimal *x2) {
         res = 1;
     else if (sign1 == 1 && sign2 == 1)
         both_negative = 1;
-    else
-        both_positive = 1;
+    // else
+    //     both_positive = 1;
 
     if (!res) {
         // приведение к одной степени
         big_decimal big1 = {0}, big2 = {0};
         init_big(*x1, &big1);
         init_big(*x2, &big2);
-        // shift(&big2);
         normalize_big(&big1, &big2);
-        print_big_decimal(&big1);
-        print_big_decimal(&big2);
-        printf("\n1) %d\t2) %d", big1.one_position_left, big2.one_position_left);
-        // проверка мантисс
-        // int dif = 0;
-        // for (int i = big1.one_position_left; i >=0; i--) {
-
-        // }
-        int scale1 = get_scale(*x1), scale2 = get_scale(*x2);
-        if (scale1 < scale2 && both_positive){
-            if (both_positive)
-                res = 1;
+        int difference_position = big1.one_position_left - big2.one_position_left;
+        // пока сравним только модули
+        if (difference_position > 0) {
+            res = 1;
+        }
+        else if (difference_position < 0){
+            res = -1;
+        }
+        else {
+            // проверка мантисс
+            int difference_bit = 0;
+            for (int i = big1.one_position_left; i >= 0; i--) {
+                difference_bit = get_bit_big(&big1, i) - get_bit_big(&big2, i);
+                if (difference_bit > 0){
+                    res = 1;
+                    break;
+                }
+                else if (difference_bit < 0) {
+                    res = -1;
+                    break;
+                }
+            }
+        }
+        // вспоминаем про знак
+        if (res) {
             if (both_negative)
-                res = -1;
+                res = -res;
         }
     }
 
@@ -60,15 +73,42 @@ int comparator(s21_decimal *x1, s21_decimal *x2) {
 }
 
 int s21_is_less(s21_decimal x1, s21_decimal x2) {
-    return comparator(&x1, &x2);
+    int res = comparator(&x2, &x1);
+    // если 1, то x2 > x1 (x1 < x2)
+    if (res == -1)
+         res = 0;
+    return res;
 }
 
-int s21_is_less_or_equal(s21_decimal x1, s21_decimal x2);
+int s21_is_less_or_equal(s21_decimal x1, s21_decimal x2) {
+    int res = comparator(&x2, &x1);
+    if (res == 0)
+        res = 1;
+    else if (res == -1)
+         res = 0;
+    return res;
+}
 
-int s21_is_greater(s21_decimal x1, s21_decimal x2);
+int s21_is_greater(s21_decimal x1, s21_decimal x2) {
+    int res = comparator(&x1, &x2);
+    if (res == -1)
+         res = 0;
+    return res;
+}
 
-int s21_is_greater_or_equal(s21_decimal x1, s21_decimal x2);
+int s21_is_greater_or_equal(s21_decimal x1, s21_decimal x2) {
+    int res = comparator(&x1, &x2);
+    if (res == 0)
+        res = 1;
+    else if (res == -1)
+         res = 0;
+    return res;
+}
 
-int s21_is_equal(s21_decimal x1, s21_decimal x2);
+int s21_is_equal(s21_decimal x1, s21_decimal x2) {
+    return (comparator(&x1, &x2)) ? 0 : 1;
+}
 
-int s21_is_not_equal(s21_decimal x1, s21_decimal x2);
+int s21_is_not_equal(s21_decimal x1, s21_decimal x2) {
+    return (comparator(&x1, &x2)) ? 1 : 0;
+}
